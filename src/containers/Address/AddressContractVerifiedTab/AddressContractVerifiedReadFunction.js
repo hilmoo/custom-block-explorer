7ace86552e1a771342de8882e2a247f4cf848961
Shaishav Pidadi <shaivpidadi@gmail.com>
2024-10-31 14:18:11 -0400

    added read functions

--- src/containers/Address/AddressContractVerifiedTab/AddressContractVerifiedReadFunction.js
+++ src/containers/Address/AddressContractVerifiedTab/AddressContractVerifiedReadFunction.js
@@ -2,76 +2,133 @@ import {
   ClipboardDocumentIcon,
   DocumentIcon,
 } from "@heroicons/react/24/outline";
-import React, { useMemo, useState } from "react";
-
+import React, { useCallback, useMemo, useState } from "react";
 // @Todo: Replace with custom tailwind
 import { Collapse } from "antd";
-import { getReadFunctions } from "../../../utils/getReadAndWriteFunctions";
-import useReadContractFunctions from "../../../hooks/useReadContractFunctions";
 import useContractFunctions from "../../../hooks/useContractFunctions";
 
-const ITEMS = [
-  {
-    key: "1",
-    label: "This panel can only be collapsed by clicking text",
-    extra: (
-      <ClipboardDocumentIcon
-        className="h-5 w-5"
-        onClick={(event) => {
-          // If you don't want click extra trigger collapse, you can prevent this:
-          event.stopPropagation();
-        }}
-      />
-    ),
-  },
-  {
-    key: "2",
-    label: "This panel can only be collapsed by clicking text",
-    extra: (
-      <ClipboardDocumentIcon
-        className="h-5 w-5"
-        onClick={(event) => {
-          // If you don't want click extra trigger collapse, you can prevent this:
-          event.stopPropagation();
-        }}
-      />
-    ),
-  },
-];
-
 const AddressContractVerifiedReadFunction = ({ address, abi }) => {
-  const [activeKey, setActiveKey] = useState(null);
-  const { readFunctions } = useContractFunctions(address, abi);
+  const { readFunctions, callReadFunction } = useContractFunctions(
+    address,
+    abi
+  );
+  const [results, setResults] = useState({});
+  const [inputValues, setInputValues] = useState({});
 
-  const getDetails = (idx, key) => {
-    return `${key}-${idx} THESE ARE DETAILS`;
+  const handleReadFunction = useCallback(
+    async (key) => {
+      const functionName = readFunctions[key].name;
+      const inputs = inputValues[key] || [];
+
+      let inputArgs = [];
+      Object.values(inputs).map((v) => inputArgs.push(v));
+      if (!results[key]) {
+        try {
+          const result = await callReadFunction(functionName, ...inputArgs);
+          setResults((prevResults) => ({ ...prevResults, [key]: result }));
+        } catch (error) {
+          console.error(`Error calling read function ${functionName}:`, error);
+          setResults((prevResults) => ({
+            ...prevResults,
+            [key]: "Error fetching result",
+          }));
+        }
+      }
+    },
+    [callReadFunction, readFunctions, results, inputValues]
+  );
+
+  const handleInputChange = (key, index, value) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [key]: {
+        ...(prevValues[key] || []),
+        [index]: value,
+      },
+    }));
   };
 
-  const onChange = (key) => {
-    setActiveKey(key);
-  };
+  const tabContent = useMemo(() => {
+    return readFunctions.map((data, idx) => ({
+      key: idx.toString(),
+      label: `${idx + 1}. ${data.name}`,
+      extra: (
+        <ClipboardDocumentIcon
+          className="h-5 w-5"
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        />
+      ),
+      children: (
+        <div>
+          {data.inputs && data.inputs.length > 0 && (
+            <div className="flex flex-col gap-2">
+              {data.inputs.map((input, i) => (
+                <input
+                  key={i}
+                  placeholder={input.name || `Input ${i + 1}`}
+                  className="p-1 border border-gray-100 rounded-md"
+                  onChange={(e) => handleInputChange(idx, i, e.target.value)}
+                />
+              ))}
 
-  console.log({ readFunctions });
+              <div className="w-[100px]">
+                <button
+                  onClick={() => handleReadFunction(idx)}
+                  className="my-2 px-4 py-0.5 bg-black text-white rounded-md"
+                >
+                  Execute
+                </button>
+              </div>
+            </div>
+          )}
+
+          {results[idx] !== undefined && (
+            <div>
+              {readFunctions[idx].outputs &&
+              readFunctions[idx].outputs.length > 0 ? (
+                readFunctions[idx].outputs.map((output, outputIdx) => (
+                  <p key={outputIdx}>
+                    {output.type}:{" "}
+                    {Array.isArray(results[idx])
+                      ? results[idx][outputIdx]?.toString()
+                      : results[idx]?.toString()}
+                  </p>
+                ))
+              ) : (
+                <p>No output</p>
+              )}
+            </div>
+          )}
+        </div>
+      ),
+    }));
+  }, [readFunctions, results, inputValues]);
+
   return (
-    <div class="flex flex-col">
-      <div className="flex align-center">
+    <div className="flex flex-col">
+      <div className="flex items-center mb-4">
         <DocumentIcon className="w-4 h-4 mr-2" />
         Supports reading the following contract function information
       </div>
 
-      <div className="grid grid-cols-1 gap-4 mt-4">
-        {ITEMS.map((item, index) => {
-          return (
+      <div className="grid grid-cols-1 gap-4">
+        {tabContent.map((item) => (
+          <div key={item.key} className="border rounded-lg shadow-md">
             <Collapse
               accordion
-              onChange={onChange}
-              collapsible="header"
-              defaultActiveKey={["1"]}
               expandIconPosition="end"
-              items={[{ ...item, children: getDetails(index, activeKey) }]}
+              onChange={() => handleReadFunction(item.key)}
+              items={[
+                {
+                  ...item,
+                  collapsible: "header",
+                },
+              ]}
             />
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
